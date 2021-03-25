36ea0b656cbb124f52082f00ff0c61e3742474a9
Vladimir Racković <vladimir.rackovic@htecgroup.com>
2021-03-25 14:21:14 +0100

    Introduced observable and simulating async calls

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay, tap } from 'rxjs/operators';
 import { Hero } from '../interfaces/hero.interface';
 import { HEROES } from '../mock-data/mock-heroes';
 
@@ -9,7 +11,13 @@ export class HeroService {
 
   constructor() { }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+
+    // Simulate async
+    // const randomDelay = Math.floor(Math.random() * 5000);
+    // const heroes = of(HEROES).pipe(delay(randomDelay), tap(val => console.log('HeroService: fetched heroes')));
+
+    return heroes;
   }
 }
